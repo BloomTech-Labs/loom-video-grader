e06f46f617be5d098a78d7a06f5db60470183105
ashtilawat23 <ashtilawat23@gmail.com>
2023-07-18 15:41:40 -0400

    init app ready

--- README.md
+++ README.md
@@ -1,11 +1,9 @@
-# BloomTech Labs Data Science API Template
+# Loom Video Grader
 
 ## Tech Stack
 - Logic: Python
 - API Framework: FastAPI
-- Validation: Pydantic
 - Database: MongoDB
-- Unit Testing: DocTest
 
 
 ## API Structure
@@ -14,14 +12,8 @@ All endpoints must return JSON compatible data.
 - API Root `/` Swagger Docs
 - API Version `/version` () -> String
   - HTTP Method: GET
-- Create User `/create-user` (User) -> Bool
-  - HTTP Method: POST
-- Read Users `/read-users` (Query) -> Array[User]
-  - HTTP Method: PUT
-- Update Users `/update-users` (Query, Update) -> Bool
-  - HTTP Method: PATCH
-- Delete Users `/delete-users` (Query) -> Bool
-  - HTTP Method: DELETE
+- Create User `/validate` (transcript) -> Bool
+  - HTTP Method: GET
 
 
 ## App Structure
@@ -29,35 +21,7 @@ All endpoints must return JSON compatible data.
   - `__init__`
   - `api.py` API File
   - `database.py` Database Interface
-  - `generators.py` Random Generators
-  - `seeds.py` DB Seed Script
-  - `utilities.py` General Tools
-  - `validation.py` Data Validation Schema
 - `.env` Environment Variables
 - `Procfile` Server Run Script
 - `requirements.txt` Dependencies
 - `run.sh` Local Run Script
-
-
-## Data Schemas
-The following classes are used to validate incoming data to the API.
-### User
-- `name` Required String (maxLength: 128 minLength: 3)
-- `age` Required Integer (maximum: 120, minimum: 1)
-- `email` Required String(EmailStr)
-- `active` Optional Boolean
-- `score` Required Float (maximum: 1, minimum: 0)
-
-### UserQuery
-- `name` Optional String (maxLength: 128 minLength: 3)
-- `age` Optional Integer (maximum: 120, minimum: 1)
-- `email` Optional String(EmailStr)
-- `active` Optional Boolean
-- `score` Optional Float (maximum: 1, minimum: 0)
-
-### UserUpdate
-- `name` Optional String (maxLength: 128 minLength: 3)
-- `age` Optional Integer (maximum: 120, minimum: 1)
-- `email` Optional String(EmailStr)
-- `active` Optional Boolean
-- `score` Optional Float (maximum: 1, minimum: 0)
--- app/api.py
+++ app/api.py
@@ -1,65 +1,64 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 
-from app.database import MongoDB
-from app.validation import User, UserQuery, UserUpdate
-from app.validation import default_query, default_update, default_user
+import openai
+from dotenv import load_dotenv
+import os
 
+with open("README.md", "r") as file:
+    next(file)
+    description = file.read()
 
+VERSION = "0.0.1"
 API = FastAPI(
-    title="BloomTech Labs DS API Template",
-    version="0.0.1",
-    docs_url="/",
+    title='Loom Validator API',
+    description=description,
+    version=VERSION,
+    docs_url='/',
 )
-API.db = MongoDB()
 API.add_middleware(
     CORSMiddleware,
-    allow_origins=["*"],
+    allow_origins=['*'],
     allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_methods=['*'],
+    allow_headers=['*'],
 )
 
-
-@API.get("/version")
-async def api_version():
-    """ Returns current API version
-    @return: String Version """
-    return API.version
+load_dotenv()
+openai.api_key = os.getenv("OPENAI_KEY")
 
 
-@API.post("/create-user")
-async def create_user(user: User = default_user):
-    """ Creates one user
-    @param user: User
-    @return: Boolean Success """
-    return API.db.create(user.dict(exclude_none=True))
+@API.get("/version", tags=["General"])
+async def version():
+    """<h3>Version</h3>
+    Returns the current version of the API
+    <pre><code>
+    @return: String </code></pre>"""
+    return VERSION
 
 
-@API.put("/read-users")
-async def read_users(user_query: UserQuery = default_query):
-    """ Returns array of all matched users
-    @param user_query: UserQuery
-    @return: Array[User] """
-    return API.db.read(user_query.dict(exclude_none=True))
-
-
-@API.patch("/update-users")
-async def update_users(user_query: UserQuery = default_query,
-                       user_update: UserUpdate = default_update):
-    """ Updates all matched users
-    @param user_query: UserQuery
-    @param user_update: UserUpdate
-    @return: Boolean Success """
-    return API.db.update(
-        user_query.dict(exclude_none=True),
-        user_update.dict(exclude_none=True),
-    )
-
-
-@API.delete("/delete-users")
-async def delete_users(user_query: UserQuery = default_query):
-    """ Deletes all matched users
-    @param user_query: UserQuery
-    @return: Boolean Success """
-    return API.db.delete(user_query.dict(exclude_none=True))
+@API.get("/validate", tags=["Loom Video Validation"])
+async def validate(transcript: str):
+    """<h3>validate</h3>
+    Returns a boolean indicating whether the transcript is a valid Loom video submission
+    <pre><code>
+    @param transcript: String
+    @return: String </code></pre>"""
+    context = "You are a master coding instructor and you are reviewing a student's Loom video submission."
+    prompt = f"Assess whether the submission was an honest attempt at solving a problem." \
+             f"Use the following video transcript to make this determination: {transcript}." \
+             f"Make sure the student actually tried to answer the question. Getting it correct is not required." \
+             f"Ensure that the student tried to explain their thought process and did not just copy and paste code." \
+             f"Make sure the student did not just submit a random video that has nothing to do with the problem." \
+             f"If you think the student made an honest effort, repond `validated` otherwise respond `not valid`."
+    result, *_ = openai.ChatCompletion.create(
+        model="gpt-3.5-turbo",
+        messages=[
+            {"role": "system", "content": context},
+            {"role": "user", "content": prompt},
+        ],
+    ).choices
+    validate_message = result.get("message").get("content").replace("\n", "<br>")
+    return {
+        "response": validate_message,
+    }
